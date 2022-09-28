b065c9d1793dbf1d148c2818cb8ae990b70256e9
Luis Fernando <luis.silva9902@gmail.com>
2022-09-27 22:20:04 -0300

    Creation of the mobile layout and finalization of the project

--- HTML e CSS/Curse/hotel job/src/js/index.js
+++ HTML e CSS/Curse/hotel job/src/js/index.js
@@ -1,1 +1,8 @@
-console.log(document.getElementById('buttonSignUp'))
+const btnMobile = document.getElementById('btn-mobile')
+
+function toggleMenu(){
+  const headerNav = document.getElementById('header-nav')
+  headerNav.classList.toggle('active')
+}
+
+btnMobile.addEventListener('click', toggleMenu)
