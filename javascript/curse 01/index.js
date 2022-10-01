f2250b7218b89d5b252a0ee783a131036a089102
Luis Fernando <luis.silva9902@gmail.com>
2022-10-01 17:48:32 -0300

    Add javascript folder

---
+++ javascript/curse 01/index.js
@@ -0,0 +1,36 @@
+// first activity
+
+// const weight = prompt('Qual o seu peso atual?')
+
+// const height = prompt('Qual a sua altura?')
+
+// const imc = ((weight / (height * 2)) * 100).toFixed(2)
+
+// const obese = imc >= 30
+
+// console.log(`O valor do IMC para o peso ${weight} quilos e altura ${height} metros é de ${imc}`)
+// console.log(obese)
+
+// second activity
+
+let number = Number(prompt('Escolha um número'))
+
+while(number > 1 ){
+    if (number % 2 === 0) {
+        number = number / 2
+    } else {
+        number = (3 * number + 1)
+    }
+    console.log(number)
+}
+
+// third activity
+
+// let heightOfChico = 150
+// let heightOfZe = 110
+
+// // while(heightOfChico > heightOfZe){
+
+// // }
+
+// console.log(abs(0.30000000000000004))
