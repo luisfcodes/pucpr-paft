eb041204d715964eb9df8e6ad072a131e0b31bf3
Luis Fernando <luis.silva9902@gmail.com>
2022-10-04 15:56:38 -0300

    Creating and styling the last challenges of class 1

--- javascript/curse 01/index.js
+++ javascript/curse 01/index.js
@@ -10,49 +10,77 @@ function calcIMC() {
   if (weight && height) {
     document.getElementById('calcImcResult').textContent = `IMC Atual`
     const resultNumberElement = document.getElementById('calcImcNumberResult')
-    if(!resultNumberElement){
-      document.getElementById('calcImcResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcImcNumberResult">${imc}</span>`) 
+    if (!resultNumberElement) {
+      document.getElementById('calcImcResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcImcNumberResult">${imc}</span>`)
     } else {
       document.getElementById('card_content_calcImc').removeChild(resultNumberElement)
-      document.getElementById('calcImcResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcImcNumberResult">${imc}</span>`) 
+      document.getElementById('calcImcResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcImcNumberResult">${imc}</span>`)
     }
   }
 }
 
 // second activity
-
-function calcCollatz(){
+function calcCollatz() {
   let number = Number(document.getElementById('numberSelected').value)
   const collatzResult = []
 
-  while(number > 1 ){
+  if (number) {
+    while (number > 1) {
       if (number % 2 === 0) {
-          number = number / 2
+        number = number / 2
       } else {
-          number = (3 * number + 1)
+        number = (3 * number + 1)
       }
       collatzResult.push(number)
-  }
+    }
 
-  if (number) {
+    function createList() {
+      document.getElementById('calcCollatzResult').insertAdjacentHTML('afterend', `<ul class="resultNumber resultNumberList" id="calcCollatzNumberResult"></ul>`)
+      collatzResult.forEach(item => {
+        let li = document.createElement("li")
+        li.innerText = item
+        li.classList.add('resultNumberItemList')
+        document.getElementById('calcCollatzNumberResult').appendChild(li)
+      })
+    }
+
     document.getElementById('calcCollatzResult').textContent = `Collatz gerado`
     const resultNumberElement = document.getElementById('calcCollatzNumberResult')
-    if(!resultNumberElement){
-      document.getElementById('calcCollatzResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcCollatzNumberResult">${collatzResult.join(" | ")}</span>`) 
+
+    if (!resultNumberElement) {
+      createList()
     } else {
       document.getElementById('card_content_calcCollatz').removeChild(resultNumberElement)
-      document.getElementById('calcCollatzResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcCollatzNumberResult">${collatzResult.join(" | ")}</span>`) 
+      createList()
     }
   }
 }
 
 // third activity
+function calcHeight() {
+  let heightOfChico = 150
+  let heightOfZe = 110
+  let yearCounter = 0
 
-// let heightOfChico = 150
-// let heightOfZe = 110
+  do {
+    heightOfChico += 2
+    heightOfZe += 3
+    yearCounter++
+  } while (heightOfChico >= heightOfZe)
 
-// // while(heightOfChico > heightOfZe){
+  function createElement(){
+    document.getElementById('calcHeightResult').innerText = `${yearCounter} anos`
+    document.getElementById('calcHeightResult').insertAdjacentHTML('beforebegin', 
+    `<div class="result" id="calcHeightDivResult">
+      <p class="calcHeightResultParagraph">Chico teria ${heightOfChico / 100} metro</p>
+      <p class="calcHeightResultParagraph">Zé teria ${heightOfZe / 100} metro</p>
+    </div>`
+    )
+  }
 
-// // }
+  const resultNumberElement = document.getElementById('calcHeightDivResult')
 
-// console.log(abs(0.30000000000000004))
+  if(!resultNumberElement){
+    createElement()
+  } 
+}
