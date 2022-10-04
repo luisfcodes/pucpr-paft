bef1156afe2fa675a334d7245aaf290e618d340c
Luis Fernando <luis.silva9902@gmail.com>
2022-10-03 23:43:29 -0300

    Home page creation, addition of global styles and lesson 01

--- javascript/curse 01/index.js
+++ javascript/curse 01/index.js
@@ -1,27 +1,49 @@
-// first activity
+//first activity
+function calcIMC() {
+  const weight = Number(document.getElementById('weight').value)
+  const height = Number(document.getElementById('height').value)
 
-// const weight = prompt('Qual o seu peso atual?')
+  const imc = ((weight / (height * 2)) * 100).toFixed(2)
 
-// const height = prompt('Qual a sua altura?')
+  const obese = imc >= 30
 
-// const imc = ((weight / (height * 2)) * 100).toFixed(2)
-
-// const obese = imc >= 30
-
-// console.log(`O valor do IMC para o peso ${weight} quilos e altura ${height} metros é de ${imc}`)
-// console.log(obese)
+  if (weight && height) {
+    document.getElementById('calcImcResult').textContent = `IMC Atual`
+    const resultNumberElement = document.getElementById('calcImcNumberResult')
+    if(!resultNumberElement){
+      document.getElementById('calcImcResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcImcNumberResult">${imc}</span>`) 
+    } else {
+      document.getElementById('card_content_calcImc').removeChild(resultNumberElement)
+      document.getElementById('calcImcResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcImcNumberResult">${imc}</span>`) 
+    }
+  }
+}
 
 // second activity
 
-let number = Number(prompt('Escolha um número'))
+function calcCollatz(){
+  let number = Number(document.getElementById('numberSelected').value)
+  const collatzResult = []
 
-while(number > 1 ){
-    if (number % 2 === 0) {
-        number = number / 2
+  while(number > 1 ){
+      if (number % 2 === 0) {
+          number = number / 2
+      } else {
+          number = (3 * number + 1)
+      }
+      collatzResult.push(number)
+  }
+
+  if (number) {
+    document.getElementById('calcCollatzResult').textContent = `Collatz gerado`
+    const resultNumberElement = document.getElementById('calcCollatzNumberResult')
+    if(!resultNumberElement){
+      document.getElementById('calcCollatzResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcCollatzNumberResult">${collatzResult.join(" | ")}</span>`) 
     } else {
-        number = (3 * number + 1)
+      document.getElementById('card_content_calcCollatz').removeChild(resultNumberElement)
+      document.getElementById('calcCollatzResult').insertAdjacentHTML('afterend', `<span class="resultNumber" id="calcCollatzNumberResult">${collatzResult.join(" | ")}</span>`) 
     }
-    console.log(number)
+  }
 }
 
 // third activity
