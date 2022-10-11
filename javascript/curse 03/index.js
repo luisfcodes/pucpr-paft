9ac829499644d3a137ca25534439f2f698631bcb
Luis Fernando <luis.silva9902@gmail.com>
2022-10-10 21:56:03 -0300

    Adding Omni theme styles and creating exercise one

--- javascript/curse 03/index.js
+++ javascript/curse 03/index.js
@@ -1,2 +1,16 @@
-console.log('a')
+// function soma(a,b){
+//   return a + b
+// }
 
+// const soma = function(a,b) {
+//   return a + b
+// }
+
+// const soma = () => a + b
+
+const soma = () => {
+  const a = Number(document.getElementById('calcSomaNumberOne').value)
+  const b = Number(document.getElementById('calcSomaNumberTwo').value)
+
+  if(a && b) document.getElementById('calcMaisResult').innerText = a + b
+}
