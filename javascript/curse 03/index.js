4888a820b44a64cdc8c016e57304fb92d8d18cc9
Luis Fernando <luis.silva9902@gmail.com>
2022-10-11 11:47:29 -0300

    Add second and third activity

--- javascript/curse 03/index.js
+++ javascript/curse 03/index.js
@@ -14,3 +14,35 @@ const soma = () => {
 
   if(a && b) document.getElementById('calcMaisResult').innerText = a + b
 }
+
+// second activity
+
+// function menos(a, b = false){
+//   return !b ? a * -1 : a - b
+// }
+
+function menos(){
+  const a = Number(document.getElementById('calcMenosNumberOne').value)
+  const b = Number(document.getElementById('calcMenosNumberTwo').value)
+
+  if(!b && a) {
+    document.getElementById('calcMenosResult').innerText = a * -1
+  } else if (a && b) {
+    document.getElementById('calcMenosResult').innerText = a - b
+  }
+}
+
+// third activity
+
+// function eCrescente(list){
+//   for(i = 1; i < list.length; i++){
+//     if(list[i] < list[i - 1])
+//       return 'Lista Não Crescente'
+//   }
+//   return 'Lista Crescente'
+// }
+
+function eCrescente(){
+  const arr = (document.getElementById('calcECrescenteList').value).split(',')
+  console.log(arr)
+}
