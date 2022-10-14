c98e04f165bbfed772d21fadb4d02090208373d5
Luis Fernando <luis.silva9902@gmail.com>
2022-10-13 23:22:09 -0300

    Add new activities

--- javascript/curse 03/index.js
+++ javascript/curse 03/index.js
@@ -110,3 +110,119 @@ function myJoin(){
     return document.getElementById('MyJoinResult').innerText = text
   }
 }
+
+// <----- sixth activity ----->
+
+function myObjectList(list){
+  const favoriteColorList = []
+  const individualColorList = []
+  
+  list.forEach(item => {
+    favoriteColorList.push(Object.getOwnPropertyDescriptor(item, 'favoriteColor').value)
+  })
+  favoriteColorList.forEach(color => {
+    if(!individualColorList.includes(color)) {
+      individualColorList.push(color)
+    }
+  })
+
+  return individualColorList
+}
+
+console.log(myObjectList([
+  {
+    name: 'Luis Fernando',
+    age: 24,
+    favoriteColor: 'azul'
+  },
+  {
+    name: 'Amanda',
+    age: 24,
+    favoriteColor: 'rosa'
+  },
+  {
+    name: 'Lorena',
+    age: 30,
+    favoriteColor: 'violeta'
+  },
+  {
+    name: 'Bryan',
+    age: 26,
+    favoriteColor: 'azul'
+  },
+]))
+
+// <----- seventh activity ----->
+
+// function fibonacciNoRecursion(n) {
+//   let a = 0
+//   let b = 1
+//   let c = n
+  
+//   for(let i = 2; i <= n; i++) {
+//     c = a + b;
+//     a = b; 
+//     b = c; 
+//   }
+  
+//   return c;
+// };
+
+
+function fibonacciWithRecursion(n){
+  if (n <= 1) return n
+
+  return fibonacciWithRecursion(n - 1) + fibonacciWithRecursion(n - 2)
+}
+
+function calcFibonacci(){
+  const number = Number(document.getElementById('calcFibonacciNumber').value)
+  if(number){
+    const fibonacciResult = fibonacciWithRecursion(number)
+    return document.getElementById('calcFibonacciResult').innerText = fibonacciResult
+  }
+}
+
+// <----- eighth activity ----->
+
+function mapear(list,func){
+  const newList = []
+
+  list.forEach(item => {
+    newList.push(func(item))
+  })
+  return newList
+}
+
+console.log(mapear([1,2,3,5,6,8], x => x * 2))
+
+// <----- ninth activity ----->
+
+function calcCollatz(n){
+  let numberCurrent = n
+  return function(){
+    if(numberCurrent <= 1) return numberCurrent
+    if(numberCurrent % 2 === 0){
+      return numberCurrent = numberCurrent / 2
+    }
+    return numberCurrent = 3 * numberCurrent + 1
+  }
+}
+
+const calcCollatzResult = calcCollatz(8)
+// console.log(calcCollatzResult())
+// console.log(calcCollatzResult())
+// console.log(calcCollatzResult())
+// console.log(calcCollatzResult())
+
+// <----- tenth activity ----->
+
+function verbose(func) {
+  return function(a,b){
+    console.log(`${func.name}(${a},${b}) = ${func(a,b)}`)
+  }
+}
+
+const somaNumbers = (a, b) => a + b;
+const sum = verbose(somaNumbers)
+sum(2,3)
