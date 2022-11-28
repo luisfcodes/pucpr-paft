e3496f9ee35e9af4a5e9375656f546fe10260b1c
Luis Fernando <luis.silva9902@gmail.com>
2022-11-27 23:33:28 -0300

    Connection with Supabase database and form validation

--- Javascript/shire-bank/src/pages/Login/index.js
+++ Javascript/shire-bank/src/pages/Login/index.js
@@ -1,60 +1,70 @@
 const container = document.querySelector('#login-container')
 
-function register() {
-  container.querySelector('h3').textContent = 'Abra sua conta agora mesmo'
-  container.querySelector('#login-content-question').innerHTML = "<p id='login-content-question'>Já possui uma conta? <button onclick='login()'>Faça login</button></p>"
-
-  container.querySelector('form').innerHTML = `
-  <div>
-    <label for="name">Nome</label>
-    <input type="text" id="name">
+function registerDOM() {
+  container.innerHTML = `
+  <div class="login-content">
+    <h3>Abra sua conta agora mesmo</h3>
+    <p id="login-content-question">Já possui uma conta? <button onclick='loginDOM()'>Faça login</button></p>
   </div>
 
-  <div>
-    <label for="email">Email</label>
-    <input type="email" id="email">
+  <form action="#" onSubmit="registerUser()">
+    <div>
+      <label for="name">Nome</label>
+      <input type="text" id="name" required>
+    </div>
+
+      <div>
+        <label for="email">Email</label>
+        <input type="email" id="email">
+      </div>
+
+      <div>
+        <label for="phone">Telefone</label>
+        <input type="tel" id="phone">
+      </div>
+
+      <div>
+        <label for="cpf">CPF</label>
+        <input type="number" id="cpf">
+      </div>
+
+      <div>
+        <label for="password">Senha</label>
+        <input type="password" id="password">
+      </div>
+
+      <div class="form-checkbox">
+        <input type="checkbox" id="term">
+        <p>Aceito os <a href="#">termos e as condições.</a></p>
+      </div>
+
+    <button>Cadastrar</button>
+  </form>
+`
+}
+
+function loginDOM() {
+  container.innerHTML = `
+  <div class="login-content">
+    <h3>Login</h3>
+    <p id="login-content-question">Não possui uma conta? <button onclick="registerDOM()">Registre-se</button></p>
   </div>
 
-  <div>
-    <label for="phone">Telefone</label>
-    <input type="tel" id="phone">
-  </div>
+  <form action="#">
+    <div>
+      <label for="email">Email</label>
+      <input type="email" id="email">
+    </div>
 
-  <div>
-    <label for="cpf">CPF</label>
-    <input type="number" id="cpf">
-  </div>
+    <div>
+      <label for="password">Senha</label>
+      <input type="password" id="password">
+    </div>
 
-  <div>
-    <label for="birthDate">Data de Nascimento</label>
-    <input type="number" id="birthDate">
-  </div>
-
-  <div class="form-checkbox">
-    <input type="checkbox">
-    <p>Aceito os <a href="#">termos e as condições.</a></p>
-  </div>
-
-  <button>Cadastrar</button>
+    <button>Entrar</button>
+    <a href="#">Esqueceu a senha?</a>
+  </form>
   `
 }
 
-function login() {
-  container.querySelector('h3').textContent = 'Login'
-  container.querySelector('#login-content-question').innerHTML = "<p id='login-content-question'>Não possui uma conta? <button onclick='register()'>Registre-se</button></p>"
-
-  container.querySelector('form').innerHTML = `
-  <div>
-    <label for="email">Email</label>
-    <input type="email" id="email">
-  </div>
-
-  <div>
-    <label for="password">Senha</label>
-    <input type="password" id="password">
-  </div>
-  
-  <button>Entrar</button>
-  <a href="#">Esqueceu a senha?</a>
-  `
-}
+loginDOM()
