9e0523e6cc98c53aea292f8842351632cbe43989
Luis <luis.silva9902@gmail.com>
2023-02-02 17:06:45 -0300

    Created project spotcar

---
+++ Ajax and API/spotCar/components/header/index.js
@@ -0,0 +1,16 @@
+document.querySelector('body').insertAdjacentHTML('afterbegin', `
+  <header>
+    <div class="container">
+      <h1>LOGO</h1>
+    
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Carros</li>
+          <li>Clientes</li>
+          <li>Sobre</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+`)
