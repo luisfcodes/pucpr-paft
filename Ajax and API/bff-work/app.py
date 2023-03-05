9fca219cef4c2df4d2221cf61f92c2371cff6e6c
Luis Fernando <luis.silva9902@gmail.com>
2023-03-05 18:14:11 -0300

    Handling errors and adding new styles and new elements

--- Ajax and API/bff-work/app.py
+++ Ajax and API/bff-work/app.py
@@ -7,23 +7,30 @@ CORS(app)
 
 @app.route('/user')
 def getUser():
-    username = request.args.get('username')
-    responseGithub = requests.get(
-        'https://api.github.com/users/'+username)
-    jsonGithub = responseGithub.json()
+    try:
+        username = request.args.get('username')
+        responseGithub = requests.get('https://api.github.com/users/'+username)
+        jsonGithub = responseGithub.json()
 
-    response = jsonGithub
-    return response
+        response = jsonGithub
+
+        return response
+    except Exception as err:
+        return err
 
 @app.route('/userData')
 def getUserData():
-    responsePokeAPI = requests.get(
-        'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
-    jsonPokeAPI = responsePokeAPI.json()
-
-    responseRickAndMortyAPI = requests.get(
-        'https://rickandmortyapi.com/api/character/?page=1')
-    jsonRickAndMortyAPI = responseRickAndMortyAPI.json()
+    try:
+        responsePokeAPI = requests.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
+        jsonPokeAPI = responsePokeAPI.json()
+    except Exception as err:
+        return err
+    
+    try:
+        responseRickAndMortyAPI = requests.get('https://rickandmortyapi.com/api/character/?page=1')
+        jsonRickAndMortyAPI = responseRickAndMortyAPI.json()
+    except Exception as err:
+        return err
 
     response = {'pokedex': jsonPokeAPI, 'rickAndMorty': jsonRickAndMortyAPI}
 
@@ -31,14 +38,29 @@ def getUserData():
 
 @app.route('/user.html')
 def showUserPage():
-    return render_template('user.html', user = [])
-    # userGithub = getUser()
-    # userData = getUserData()
+    pokemonList = []
+    rickAndMortyList = []
+    userGithub = getUser()
+    userData = getUserData()
 
-    # return render_template('user.html', user = {
-    #     'user': userGithub,
-    #     'pokedex': userData['pokedex'],
-    #     'rickAndMorty': userData['rickAndMorty'],
-    # })
+    for pokemon in userData['pokedex']['results']:
+        pokemonData = requests.get(pokemon['url'])
+        jsonPokemonData = pokemonData.json()
+        pokemonList.append({
+            'name': jsonPokemonData['name'],
+            'imageUrl': jsonPokemonData['sprites']['other']['dream_world']['front_default']
+        })
+
+    for character in userData['rickAndMorty']['results']:
+        rickAndMortyList.append({
+            'name': character['name'],
+            'imageUrl': character['image']
+        })
+
+    return render_template('user.html', user = {
+        'user': userGithub,
+        'pokedex': pokemonList,
+        'rickAndMorty': rickAndMortyList,
+    })
     
 app.run(debug=True)
--- Ajax and API/bff-work/script.js
+++ Ajax and API/bff-work/script.js
@@ -1,13 +1,76 @@
 async function handleSubmit(event){
   event.preventDefault()
+
+  document.querySelector('#loader').classList.remove('hidden')
+  document.querySelector('#span-button-search').classList.add('hidden')
   
   const user = document.querySelector('#username').value
+  const divGithubElement = document.querySelector('#div-user-github')
+  const spanGithubElement = document.querySelector('#span-user-github')
+  const divPokedexElement = document.querySelector('#div-pokedex')
+  const spanPokedexElement = document.querySelector('#span-pokedex')
+  const divRickAndMortyElement = document.querySelector('#div-rickAndMorty')
+  const spanRickAndMortyElement = document.querySelector('#span-rickAndMorty')
 
-  let userData = await fetch(`http://127.0.0.1:5000/user?username=${user}`)
-  userData = await userData.json()
+  try {
+    let userData = await fetch(`http://127.0.0.1:5000/user?username=${user}`)
+    userData = await userData.json()
 
-  let pokedexData = await fetch(`http://127.0.0.1:5000/userData?username=${user}`)
-  pokedexData = await pokedexData.json()
+    if(userData.message !== "Not Found"){
+      divGithubElement.classList.add('success')
+      spanGithubElement.textContent = "Usuário encontrado com sucesso!!!"
+      divGithubElement.classList.remove('hidden', 'warning')
 
-  window.location.href = `http://127.0.0.1:5000/user.html?username=${user}`
+      setTimeout(async () => {
+        try {
+          let loadData = await fetch(`http://127.0.0.1:5000/userData`)
+          loadData = await loadData.json()
+      
+          const pokedexData = loadData.pokedex
+          const rickAndMortyData = loadData.rickAndMorty
+      
+          if(pokedexData.results.length){
+            divPokedexElement.classList.add('success')
+            spanPokedexElement.textContent = "Pokedex carregada com sucesso!!!"
+            divPokedexElement.classList.remove('hidden', 'warning')
+          } else {
+            spanPokedexElement.textContent = "Falha ao carregar Pokedex!!!"
+            divPokedexElement.classList.add('warning')
+            divPokedexElement.classList.remove('hidden', 'success')
+  
+            throw new Error
+          }
+      
+          setTimeout(() => {
+            if(rickAndMortyData.results.length){
+              divRickAndMortyElement.classList.add('success')
+              spanRickAndMortyElement.textContent = "Série favorita carregada com sucesso!!!"
+              divRickAndMortyElement.classList.remove('hidden', 'warning')
+            } else {
+              spanRickAndMortyElement.textContent = "Falha ao carregar Pokedex!!!"
+              divRickAndMortyElement.classList.add('warning')
+              divRickAndMortyElement.classList.remove('hidden', 'success')
+    
+              throw new Error
+            }
+          }, 1000)
+
+          setTimeout(() => {
+            window.location.href = `http://127.0.0.1:5000/user.html?username=${user}`
+          }, 500)
+        } catch (err){
+          console.log(err)
+        }
+      }, 1000)
+    } else {
+      spanGithubElement.textContent = "Usuário não encontrado!!!"
+      divGithubElement.classList.add('warning')
+      divGithubElement.classList.remove('hidden', 'success')
+      throw new Error
+    }
+  } catch (err){
+    document.querySelector('#loader').classList.add('hidden')
+    document.querySelector('#span-button-search').classList.remove('hidden')
+    console.log(err)
+  }
 }
